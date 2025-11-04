f9412aa2dd99a589d8530ba721055e6b563e77ac
Rus1IT <ruslankoldasov1@gmail.com>
2025-11-04 20:14:34 +0500

    Initializing service

---
+++ src/main/java/com/rus1it/transactionservicecashpilot/TransactionServiceCashpilotApplication.java
@@ -0,0 +1,13 @@
+package com.rus1it.transactionservicecashpilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionServiceCashpilotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionServiceCashpilotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rus1it/transactionservicecashpilot/TransactionServiceCashpilotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rus1it.transactionservicecashpilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionServiceCashpilotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
